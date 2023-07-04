ea35d3bcd723b901cd307b50c3143a14a8ceb2c3
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2023-07-04 12:17:32 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,6 +5,9 @@ from app import app, db
 
 @pytest.fixture
 def test_client():
+    """
+    Fixture that provides a test client for the Flask application.
+    """
     app_context = app.app_context()
     app_context.push()
     init_db()
@@ -14,42 +17,62 @@ def test_client():
 
 
 def init_db():
+    """
+    Initialize the test database.
+    """
     DATABASE = 'test_emp_db.db'
     app.config.update(
-        SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + DATABASE
     )
 
 
 def truncate_db():
+    """
+    Truncate the test database.
+    """
     with app.app_context():
         models.Employee.query.delete()
         db.session.commit()
 
 
 def test_index(test_client):
+    """
+    Test the index route.
+    """
     response = test_client.get('/')
     assert response.status_code == 200
 
 
 def test_add(test_client):
-    test_data = {'name': 'Mickey Test',
-                 'gender': 'male',
-                 'address': 'IN',
-                 'phone': '0123456789',
-                 'salary': '2000',
-                 'department': 'Sales'}
+    """
+    Test the add route.
+    """
+    test_data = {
+        'name': 'Mickey Test',
+        'gender': 'male',
+        'address': 'IN',
+        'phone': '0123456789',
+        'salary': '2000',
+        'department': 'Sales'
+    }
     test_client.post('/add', data=test_data)
     with app.app_context():
         assert models.Employee.query.count() == 1
 
 
 def test_edit(test_client):
+    """
+    Test the edit route.
+    """
     response = test_client.post('/edit/0')
     assert response.status_code == 200
     assert b"Sorry, the employee does not exist." in response.data
 
 
 def test_delete(test_client):
+    """
+    Test the delete route.
+    """
     test_data = {'emp_id': 0}
     response = test_client.post('/delete', data=test_data)
     assert response.status_code == 200
