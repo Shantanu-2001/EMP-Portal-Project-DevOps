9a6d205e6734717b163226604c6a5fcacdc326b7
Shantanurana <78650181+Shantanu-2001@users.noreply.github.com>
2023-07-04 12:07:00 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,7 @@
+"""
+Module for testing the application.
+"""
+
 import pytest
 from models import models
 from app import app, db
@@ -5,47 +9,66 @@ from app import app, db
 
 @pytest.fixture
 def client():
+    """
+    Fixture for setting up and tearing down the test environment.
+    """
     app_context = app.app_context()
     app_context.push()
-    initDB()
+    init_db()
     yield app.test_client()
-    truncateDB()
+    truncate_db()
     app_context.pop()
 
 
-def initDB():
-    DATABASE = 'test_emp_db.db'
+def init_db():
+    """
+    Initialize the test database.
+    """
+    database = 'test_emp_db.db'
     app.config.update(
-        SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + database
     )
 
 
-def truncateDB():
+def truncate_db():
+    """
+    Truncate the test database.
+    """
     with app.app_context():
         models.Employee.query.delete()
         db.session.commit()
 
 
 def test_index():
+    """
+    Test case for the index route.
+    """
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
 
 
-
 def test_add(client):
-    test_data = {'name': 'Mickey Test',
-                 'gender': 'male',
-                 'address': 'IN',
-                 'phone': '0123456789',
-                 'salary': '2000',
-                 'department': 'Sales'}
+    """
+    Test case for adding an employee.
+    """
+    test_data = {
+        'name': 'Mickey Test',
+        'gender': 'male',
+        'address': 'IN',
+        'phone': '0123456789',
+        'salary': '2000',
+        'department': 'Sales'
+    }
     client.post('/add', data=test_data)
     with app.app_context():
         assert models.Employee.query.count() == 1
 
 
 def test_edit():
+    """
+    Test case for editing an employee.
+    """
     client = app.test_client()
     response = client.post('/edit/0')
     assert response.status_code == 200
@@ -53,6 +76,9 @@ def test_edit():
 
 
 def test_delete(client):
+    """
+    Test case for deleting an employee.
+    """
     test_data = {'emp_id': 0}
     response = client.post('/delete', data=test_data)
     assert response.status_code == 200
